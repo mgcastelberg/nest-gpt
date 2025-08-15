da65e7615350702eab7080e3f9a91890989d3017
ManuxDark <mgcastelberg@hotmail.com>
2025-08-14 20:12:39 -0600

    OpenAI - Configuración de cuenta y token

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -6,8 +6,9 @@ interface Options {
 export const orthographyCheckUseCase = async (options: Options) => {
 
     const { prompt } = options;
-
+    // process.env.OPENAI_API_KEY
     return {
-        prompt: prompt
+        prompt: prompt,
+        apikey: 'api-key',
     }
 };
